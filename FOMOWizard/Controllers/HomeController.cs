c3d6329826261a92ba6099cd11086f2b793569f5
Kwang Wei Tan <s10186878@connect.np.edu.sg>
2020-03-17 11:18:33 +0800

    Add project files.

---
+++ FOMOWizard/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace FOMOWizard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StaffLogin(IFormCollection formData)
+        {
+            // Read inputs from textboxes // Email address converted to lowercase 
+            string loginID = formData["txtLoginID"].ToString().ToLower();
+            string password = formData["txtPassword"].ToString();
+
+            if (loginID == "abc@gmail.com" && password == "pass1234")
+            {
+                // Store Login ID in session with the key as “LoginID” 
+                HttpContext.Session.SetString("LoginID", loginID);
+                // Store user role “Staff” in session with the key as “Role” 
+                HttpContext.Session.SetString("Role", "Staff");
+                // Redirect user to the "StaffMain" view through an action 
+                return RedirectToAction("StaffMain");
+            }
+            else
+            {
+                // Store an error message in TempData for display at the index view 
+                TempData["Message"] = "Invalid Login Credentials!";
+                // Redirect user back to the index view through an action 
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            // Clear all key-values pairs stored in session state 
+            HttpContext.Session.Clear();
+            // Call the Index action of Home controller 
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult StaffMain()
+        {
+            return View();
+        }
+    }
+}
---
+++ FOMOWizard/DAL/StaffDAL.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FOMOWizard.DAL
+{
+    public class StaffDAL
+    {
+        private IConfiguration Configuration { get; set; }
+        private SqlConnection conn;
+        //Constructor
+        public StaffDAL()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            //Read ConnectionString from appsettings.json file 
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString("NPBookConnectionString");
+            //Instantiate a SqlConnection object with the Connection String read. 
+            conn = new SqlConnection(strConn);
+        }
+    }
+}
